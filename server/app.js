a33104e404dcd568e31f0f36746de235cb9ca439
Nazar <horodyskyinazarr@gmail.com>
2017-10-22 18:38:42 +0300

    Add Project

--- server/app.js
+++ server/app.js
@@ -1,11 +1,31 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+import {serverPort} from '../etc/config.json';
+
+import * as db from './utils/DataBaseUtils.js';
+
+db.setUpConnection();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello word');
+app.use(bodyParser.json());
+
+app.use(cors({ origin: '*' }));
+
+app.get('/notes', (req, res) => {
+  db.listNotes().then(data => res.send(data));
 });
 
-const server = app.listen(8080, () => {
-  console.log('Server is runing');
+app.post('/notes', (req, res) => {
+  db.createNote(req.body).then(data => res.send(data));
+});
+
+app.delete('/notes/:id', (req, res) => {
+  db.deleteNote(req.params.id).then(data => res.send(data));  
+});
+
+const server = app.listen(serverPort, () => {
+  console.log(`Server is up and runing on port ${serverPort}`);
 });
--- server/models/Note.js
+++ server/models/Note.js
@@ -2,6 +2,10 @@ import mongoose from 'mongoose';
 
 const Schema = mongoose.Schema;
 
-const NoteSchma = new Schema({
-  title: { type: Strimg}
-})
+const NoteSchema = new Schema({
+  title: { type: String },
+  text: { type: String, required: true },
+  color: { type: String },
+  createdAt: { type: Date }
+});
+const Note = mongoose.model('Note', NoteSchema);
